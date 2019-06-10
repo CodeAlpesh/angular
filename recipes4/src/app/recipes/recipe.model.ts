a689eae9aa654436f03eda6ce98a29937bd8fe79
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-06-10 17:36:05 +0530

    Show Ingredients. - Created ingredients model - display name/amount

--- recipes4/src/app/recipes/recipe.model.ts
+++ recipes4/src/app/recipes/recipe.model.ts
@@ -1,10 +1,10 @@
 export class Recipe {
+    
     public name: String;
     public description: String;
     public imagePath: String;
 
-    constructor(name
-        : String, desc: String, path: String) {
+    constructor(name: String, desc: String, path: String) {
         this.name = name;
         this.description = desc;
         this.imagePath = path;
--- recipes4/src/app/shopping-list/shopping-list.component.ts
+++ recipes4/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Bananas', 6),
+    new Ingredient('Tomotoes', 3)
+  ];
+  
   constructor() { }
 
   ngOnInit() {
