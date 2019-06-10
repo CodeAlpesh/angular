c744cce55a79003eb01a8666ced72ffa2cb3a0c9
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-06-10 15:50:09 +0530

    Display list of recipes. 1. Created recipe model. 2. Markup for diplaying recipes. TODO: Need to move recipe markup to recipe-item component.

--- recipes4/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes4/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe("Tea", "Brewed to experience the magic.", "https://tse4.mm.bing.net/th?id=OIP.d8hkH1YbN8-Vt4tAAuuYLgHaFj&pid=Api&rs=1&p=0"),
+    new Recipe("Coffee", "Brewed to experience the magic.", "https://c.pxhere.com/photos/34/bf/coffee_glass_beverage_coffee_mug_caffeine_drink_coffee_shop_atmosphere-549599.jpg!d"),
+    new Recipe("LemoTea", "Brewed to experience the magic.", "https://pixnio.com/free-images/2018/10/05/2018-10-05-20-49-37-1200x789.jpg")
+  ]
+  
   constructor() { }
 
   ngOnInit() {
