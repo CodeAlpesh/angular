a2d80541bad70738ac0b8c153e8036d086137f5d
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-06-13 17:41:33 +0530

    recipe-item component will handle the presentation of recipe. Using custom property to pass recipe details to this component.

--- recipes4/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes4/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("Tea", "Brewed to experience the magic.", "https://tse4.mm.bing.net/th?id=OIP.d8hkH1YbN8-Vt4tAAuuYLgHaFj&pid=Api&rs=1&p=0"),
     new Recipe("Coffee", "Brewed to experience the magic.", "https://c.pxhere.com/photos/34/bf/coffee_glass_beverage_coffee_mug_caffeine_drink_coffee_shop_atmosphere-549599.jpg!d"),
-    new Recipe("LemoTea", "Brewed to experience the magic.", "https://pixnio.com/free-images/2018/10/05/2018-10-05-20-49-37-1200x789.jpg")
+    new Recipe("LemoTea", "Brewed to experience the magic.", "https://4.imimg.com/data4/GC/RY/MY-2067796/lemon-tea-500x500.png")
   ]
   
   constructor() { }
