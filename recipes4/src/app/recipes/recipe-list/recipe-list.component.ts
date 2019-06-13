0f5f4ecac36b01b69f6f07b26d2911ea19d99418
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-06-13 20:31:01 +0530

    Data-binding. Display recipe details for selected recipe item.

--- recipes4/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes4/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,9 +14,15 @@ export class RecipeListComponent implements OnInit {
     new Recipe("LemoTea", "Brewed to experience the magic.", "https://4.imimg.com/data4/GC/RY/MY-2067796/lemon-tea-500x500.png")
   ]
   
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
