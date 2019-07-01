65da18e5ca8ae5467b088f16d4d6f3c19d0e2a1b
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-01 15:09:10 +0530

    Add recipe ingredient to shopping list. Using - Dependency injection for services. - Use spread operator (...) to add each element of an array to existing array.

--- recipes4/src/app/recipes/recipe.model.ts
+++ recipes4/src/app/recipes/recipe.model.ts
@@ -1,13 +1,17 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     
     public name: String;
     public description: String;
     public imagePath: String;
+    public ingredients: Ingredient[];
 
-    constructor(name: String, desc: String, path: String) {
+    constructor(name: String, desc: String, path: String, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = path;
+        this.ingredients = ingredients;
     }
     
 }
--- recipes4/src/app/recipes/recipe.service.ts
+++ recipes4/src/app/recipes/recipe.service.ts
@@ -1,18 +1,56 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("Tea", "Brewed to experience the magic.", "https://tse4.mm.bing.net/th?id=OIP.d8hkH1YbN8-Vt4tAAuuYLgHaFj&pid=Api&rs=1&p=0"),
-        new Recipe("Coffee", "Brewed to experience the magic.", "https://c.pxhere.com/photos/34/bf/coffee_glass_beverage_coffee_mug_caffeine_drink_coffee_shop_atmosphere-549599.jpg!d"),
-        new Recipe("LemoTea", "Brewed to experience the magic.", "https://4.imimg.com/data4/GC/RY/MY-2067796/lemon-tea-500x500.png")
+        new Recipe(
+            "Tea", 
+            "Brewed to experience the magic.", 
+            "https://tse4.mm.bing.net/th?id=OIP.d8hkH1YbN8-Vt4tAAuuYLgHaFj&pid=Api&rs=1&p=0",
+            [
+                new Ingredient("Tea Leaves", 1),
+                new Ingredient("Sugar", 1),
+                new Ingredient("Spices", 1),
+                new Ingredient("Milk", 1)
+            ]
+        ),
+        new Recipe(
+            "Coffee", 
+            "Brewed to experience the magic.", 
+            "https://c.pxhere.com/photos/34/bf/coffee_glass_beverage_coffee_mug_caffeine_drink_coffee_shop_atmosphere-549599.jpg!d",
+            [
+                new Ingredient("Coffee", 1),
+                new Ingredient("Sugar", 1),
+                new Ingredient("Milk", 1)
+            ]
+        ),
+        new Recipe(
+            "Lemon Tea", 
+            "Brewed to experience the magic.", 
+            "https://4.imimg.com/data4/GC/RY/MY-2067796/lemon-tea-500x500.png",
+            [
+                new Ingredient("Tea Leaves", 1),
+                new Ingredient("Honey", 1),
+                new Ingredient("Lemon", 1),
+                new Ingredient("Hot Water", 1)
+            ]
+        )
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 
 }
