7d95b4e40bbd089c476416613a870c8521155626
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-05 11:44:03 +0530

    Use wildcard to match all routes. Then redirect to not-found.

--- CodeExamples/11_routing-start/src/app/app.module.ts
+++ CodeExamples/11_routing-start/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
   {path: "", component: HomeComponent, pathMatch: "full" },
@@ -20,7 +21,9 @@ const appRoutes: Routes = [
   {path: "servers", component: ServersComponent, children: [
     {path: ":id", component: ServerComponent },
     {path: ":id/edit", component: EditServerComponent }
-  ] },
+  ]},
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -31,7 +34,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
