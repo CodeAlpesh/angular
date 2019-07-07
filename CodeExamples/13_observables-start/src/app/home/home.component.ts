770533b94c53acda15f201829fe949c09d257366
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-07 16:08:32 +0530

    Using built in observer inerval. If not subscribed then ... it wil keep writing on console.
    Each component initilaizaion will add new stream ...
    This is memory leak.

--- CodeExamples/13_observables-start/src/app/home/home.component.ts
+++ CodeExamples/13_observables-start/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private intervalSunscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.intervalSunscription = interval(2000).subscribe(
+      (count: number) => {
+        console.log(count);
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    this.intervalSunscription.unsubscribe();
+  }
 }
