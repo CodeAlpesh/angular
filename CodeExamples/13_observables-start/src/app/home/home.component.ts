a5fb447e453deb6fb260ebd58f64cd5723eef69e
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-07 16:10:25 +0530

    Writing custom Observable with same behaviour.

--- CodeExamples/13_observables-start/src/app/home/home.component.ts
+++ CodeExamples/13_observables-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval } from 'rxjs'
+import { Subscription, Observable, Subscriber } from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -8,19 +8,34 @@ import { interval } from 'rxjs'
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  private intervalSunscription;
+  private intervalSunscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+
+  const myObservable:Observable<number> = Observable.create((observer: Subscriber<number>) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+  });
+  this.intervalSunscription = myObservable.subscribe((data: number) => {
+    console.log(data);
+  })
+
+    /*
     this.intervalSunscription = interval(2000).subscribe(
       (count: number) => {
         console.log(count);
       }
     );
+    */
   }
 
   ngOnDestroy(): void {
     this.intervalSunscription.unsubscribe();
   }
+
 }
