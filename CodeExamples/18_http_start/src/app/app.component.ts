78dd61e40ffab211290bd1ddcd62836291dfd7d4
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-14 10:30:58 +0530

    Display posts retrieved from server.

--- CodeExamples/18_http_start/src/app/app.component.ts
+++ CodeExamples/18_http_start/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts:Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -52,8 +52,9 @@ export class AppComponent implements OnInit {
       }
     ))
     .subscribe(
-      (responseData) => {
-        console.log(responseData);  
+      (responseData: Post[]) => {
+        console.log(responseData);
+        this.loadedPosts = responseData;  
       }
     )
   }
