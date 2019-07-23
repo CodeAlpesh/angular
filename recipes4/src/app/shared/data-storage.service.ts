15bd019ee7f08b0f226ccb98a4c89b5d41c114fb
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-23 18:28:07 +0530

    Use ondemand subscription in service and replace Observable in chain using BehaviourSubject and take, exhastMap operators.

--- recipes4/src/app/shared/data-storage.service.ts
+++ recipes4/src/app/shared/data-storage.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { map, tap, catchError } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { map, tap, catchError, take, exhaustMap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { throwError } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
 
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(
+        private http: HttpClient, 
+        private recipeService: RecipeService,
+        private authService: AuthService) {}
 
     saveData() {
         const recipes = this.recipeService.getRecipes();
@@ -26,9 +30,32 @@ export class DataStorageService {
     }
 
     fetchData() {
-        return this.http.
-            get<Recipe[]>('https://recipebook-36c22.firebaseio.com/recipes.json')
-            .pipe(
+
+        // I do not want to maintain a subscription to Subject in such cases. I need data on demand.
+        // When i need data on demand: I need last event that occured. 
+        // Use Behaviour subject and use take operator to retrieve last event data.
+        
+        /*
+        this.authService.user.pipe(take(1)).subscribe((user)=> { 
+            //the take operator will unsubscribe also.
+            //But I can not return hhtp.get observable ffrom here. It should be returned at top level.
+            //use exhastMap ... it will resolve existing Observable (i.e. extract value) and replace the Observable in chain.
+        })
+        */
+        return this.authService.user.
+            pipe(
+                //subscribe, take value and unsubscribe
+                take(1),
+                //replace with new Observable
+                exhaustMap((user) => {
+                    return this.http.
+                        get<Recipe[]>(
+                            'https://recipebook-36c22.firebaseio.com/recipes.json',
+                            {
+                                params: new HttpParams().set("auth", user.token)
+                            }
+                        )
+                }),
                 tap((recipes) => {
                     //no need to return
                     console.log(recipes);    
@@ -54,6 +81,5 @@ export class DataStorageService {
                     return throwError(errorResponse);
                 })
             );
-            
     }
 }
