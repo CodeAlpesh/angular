ae2c3cb6a6b79597f2252cb86e6482bcc8fdd625
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-25 12:50:14 +0530

    USing CanActivate guard to check auth status. Naviagate to /auth if not logged in. Router will subscribe to this Observable and will wait for it to get resolved. I have not subscribed using take/subscribe method. Angular will subscribe. Same as Resolve ...

--- recipes4/src/app/app-routing.module.ts
+++ recipes4/src/app/app-routing.module.ts
@@ -8,11 +8,12 @@ import { RecipeResolver } from './recipes/reipe-resolver-guard.service';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ResipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
     {path: '',  redirectTo: 'recipes', pathMatch: 'full' },
     {path: 'auth', component: AuthComponent},
-    {path: 'recipes', component: RecipesComponent, children:[
+    {path: 'recipes', component: RecipesComponent, canActivate:[AuthGuard], children:[
         {path: 'new', component: RecipeEditComponent },
         // {path: ':id', component: RecipeDetailsComponent, resolve: { recipe : RecipeResolver }},
         {path: ':id', component: RecipeDetailsComponent, resolve: [ResipesResolverService] },
