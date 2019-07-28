31c3f2e91d16e57f06df838ecc321a013673000a
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-07-28 12:17:43 +0530

    Bug Fix | Too many network calls. For below route changes, the resolver was making a http call. /recipes/id /recipes/id/edit

--- recipes4/src/app/recipes/recipes-resolver.service.ts
+++ recipes4/src/app/recipes/recipes-resolver.service.ts
@@ -5,15 +5,23 @@ import { catchError } from 'rxjs/operators';
 
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class ResipesResolverService implements Resolve<Recipe[]> {
 
-    constructor(private dataStorageService: DataStorageService) { }
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
     
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
         Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
         
+        // Bug Fix: For each route access to /recipes/edit/1, /recipes/1 ... the resolver was fetching recipes.
+        // It was too many calls and slowing down the view change.
+        const recipes = this.recipeService.getRecipes();
+        if(recipes && recipes.length > 0) {
+            return of([]); 
+        }
+
         //return an Observable / Promise / Recipe Array
         //In case of Observable / Promise ... resolver/angular will subscribe to Observable / Promise.
         return this.dataStorageService.fetchData().pipe(
