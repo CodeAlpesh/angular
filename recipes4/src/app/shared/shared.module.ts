117be5bf57fe2502abd6229ec4794fcf58491850
Alpesh Parekh <mailtoalpesh@gmail.com>
2019-08-01 14:16:43 +0530

    Added comments for shared module.

--- recipes4/src/app/shared/shared.module.ts
+++ recipes4/src/app/shared/shared.module.ts
@@ -18,5 +18,13 @@ import { CommonModule } from '@angular/common';
     ]
 })
 export class SharedModule {
-
 }
+/**
+ * There can be many shared modules ... Can be named better as per whats is grouped/bundled in to it.
+ * Since it is meant to be reusable (used by other modules), 
+ * => Export all declaration
+ * => Export all modules required by self (like CommonModule), so that imported do not have to manage it.
+ * 
+ * Module can be imported in many other modules.
+ * Component can be declared in only One module. 
+ */
